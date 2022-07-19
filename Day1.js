5ad7ca0ad475004994159b33f1cc54fea432d7cc
Harshita Singh <73538870+ATIHSRAH@users.noreply.github.com>
2022-07-19 18:53:21 +0530

    Update Day1.js

--- Day1.js
+++ Day1.js
@@ -1,10 +1,23 @@
-
-function minimumWaitingTime(queries) {
-    queries = queries.sort((a, b) => a - b); // nlogn - time taken by sorting algorithm
-    let total = 0
-
-    for (let i = 0; i < queries.length; i++) { // n for the loop on queries
-        total += queries[i] * (queries.length - (i + 1))
+function maxProfitWithKTransactions(prices, k) {
+  if (!prices.length) return 0;
+  const evenProfits = new Array(prices.length).fill(0);
+  const oddProfits = new Array(prices.length).fill(0);
+  for (let t = 1; t < k + 1; t++) {
+    let maxThusFar = -Infinity;
+    let currentProfits, previousProfits;
+    if (t % 2 === 1) {
+      currentProfits = oddProfits;
+      previousProfits = evenProfits;
+    } else {
+      currentProfits = evenProfits;
+      previousProfits = oddProfits;
     }
-  return total;
+    for (let d = 1; d < prices.length; d++) {
+      maxThusFar = Math.max(maxThusFar, previousProfits[d - 1] - prices[d - 1]);
+      currentProfits[d] = Math.max(currentProfits[d - 1], maxThusFar + prices[d]);
+    }
+  }
+  return k % 2 === 0 ? evenProfits[prices.length - 1] : oddProfits[prices.length - 1];
 }
+
+exports.maxProfitWithKTransactions = maxProfitWithKTransactions;
